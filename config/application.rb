1dc77ddc04f0d1fb58aa04d8ede3e0c4cf12b26d
Nicole Maneth <nmaneth@pplusadvisors.com>
2016-06-13 10:52:20 -0500

    oops

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,5 @@ module WichitaWeightTexts
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
-
-    protect_from_forgery with: :exception
-    
   end
 end
